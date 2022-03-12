9d387c982436274bd7daedd8643792cc8ea59544
song <3224266014@qq.com>
2022-03-12 10:38:11 +0800

    🐞fix: db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 'use strict';
 
 /** MONGODB **/
-// const mongodbUrl = 'mongodb://song:242166@127.0.0.1:27017/der-cli?authSource=admin';
 const mongodbUrl = 'mongodb://127.0.0.1:27017/der-cli';
 const mongodbDbName = 'der-cli';
 
